d48d3f43e1b22930f6395c043929cfe197f8074b
PabloZuVal2 <pablo.zuniga.vale@gmail.com>
2019-11-21 18:24:40 -0300

    calendario terminado

--- back-end/models/evento.js
+++ back-end/models/evento.js
@@ -3,8 +3,8 @@ import mongoose,{Schema} from 'mongoose';
 const eventoSchema = new Schema({
     name: { type: String, maxlength: 50, unique: false, required: true},
     details:{type:String, maxlength:125, required:true},
-    start:{type:Date, unique: false, required: true},
-    end:{type:Date,unique: false, required: true},
+    start:{type:String, unique: false, required: true},
+    end:{type:String,unique: false, required: true},
     color:{type:String, maxlength:10, required:true}
 });
 const Evento = mongoose.model('evento',eventoSchema);
